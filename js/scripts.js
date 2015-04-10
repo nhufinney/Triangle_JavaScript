39b073f745357ad97093922908287e7db3debd78
nhu finney <nhufinney@gmail.com>
2015-04-10 09:25:30 -0700

    passed the first test.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
-var triangle = function(input) {
+var triangle = function(side_a, side_b, side_c) {
 
+    //check a combination of 3 lengths to form a triangle
+    var result ="";
+    if ((side_a + side_b > side_c) && (side_a + side_c > side_b) && (side_b + side_c > side_a)){
+        result = "scalene";
+    } else { result= "not triangle";}
+
+    
+
+
+    return result;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,9 +1,7 @@
 describe('triangle', function(){
 
-    it("it.... ", function(){
-        expect(triangle(1 , 8, 2)).to.eql("...");
+    it("it returns 3 lengthes do not form a triangle", function(){
+        expect(triangle(1 , 8, 2)).to.equal("not triangle");
     });
-
-    
 
 });
