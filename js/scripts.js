f27135efe12e7a177c944357eaad5a2b4c96a01f
nhu finney <nhufinney@gmail.com>
2015-04-10 10:09:54 -0700

    passed all tests.

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,18 @@ var triangle = function(side_a, side_b, side_c) {
 
     //check a combination of 3 lengths to form a triangle
     var result ="";
+
+
     if ((side_a + side_b > side_c) && (side_a + side_c > side_b) && (side_b + side_c > side_a)){
-        result = "scalene";
         if ((side_a === side_b) || (side_a === side_c) || (side_b === side_c)){
-            result = "isosceles";
-        }
-    } else { result= "not triangle";}
-
-    
-
+            if ((side_a === side_b) && (side_b === side_c)){
+                result = "equilateral";
+            }
+            else {
+                result = "isosceles";
+            }} else {
+        result = "scalene";
+    }} else { result = "not triangle"}
 
     return result;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -3,12 +3,16 @@ describe('triangle', function(){
     it("it returns 3 lengthes do not form a triangle", function(){
         expect(triangle(2 , 8, 3)).to.equal("not triangle");
     });
-
+    
     it("it returns 3 lengthes form a normal triangle: scalene", function(){
         expect(triangle(5 , 8, 7)).to.equal("scalene");
     });
 
     it("it returns 3 lengthes form an isosceles triangle", function(){
             expect(triangle(5 , 8, 5)).to.equal("isosceles");
-        });
+    });
+
+    it("it returns 3 lengthes form an equilateral triangle", function(){
+            expect(triangle(5 , 5, 5)).to.equal("equilateral");
+    });
 });
