a37494d216bc1fcf36bfce32ed5d5354bdd5feab
nhu finney <nhufinney@gmail.com>
2015-04-10 10:34:13 -0700

    finish all files. changes tests spect to compliance with the final result displays.

--- js/scripts.js
+++ js/scripts.js
@@ -7,13 +7,31 @@ var triangle = function(side_a, side_b, side_c) {
     if ((side_a + side_b > side_c) && (side_a + side_c > side_b) && (side_b + side_c > side_a)){
         if ((side_a === side_b) || (side_a === side_c) || (side_b === side_c)){
             if ((side_a === side_b) && (side_b === side_c)){
-                result = "equilateral";
+                result = "an equilateral";
             }
             else {
-                result = "isosceles";
+                result = "an isosceles";
             }} else {
-        result = "scalene";
-    }} else { result = "not triangle"}
+        result = "a scalene";
+    }} else { result = "not a triangle"}
 
     return result;
 };
+
+$(document).ready(function() {
+    $("form#triangle").submit(function(event) {
+        var input1 = parseInt($('input#input1').val());
+        var input2 = parseInt($('input#input2').val());
+        var input3 = parseInt($('input#input3').val());
+
+        var result = triangle(input1, input2, input3);
+
+
+        $("#result").text("It is "+result);
+        
+
+        $("#result").show();
+
+        event.preventDefault();
+    });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,18 +1,18 @@
 describe('triangle', function(){
 
     it("it returns 3 lengthes do not form a triangle", function(){
-        expect(triangle(2 , 8, 3)).to.equal("not triangle");
+        expect(triangle(2 , 8, 3)).to.equal("not a triangle");
     });
-    
+
     it("it returns 3 lengthes form a normal triangle: scalene", function(){
-        expect(triangle(5 , 8, 7)).to.equal("scalene");
+        expect(triangle(5 , 8, 7)).to.equal("a scalene");
     });
 
     it("it returns 3 lengthes form an isosceles triangle", function(){
-            expect(triangle(5 , 8, 5)).to.equal("isosceles");
+            expect(triangle(5 , 8, 5)).to.equal("an isosceles");
     });
 
     it("it returns 3 lengthes form an equilateral triangle", function(){
-            expect(triangle(5 , 5, 5)).to.equal("equilateral");
+            expect(triangle(5 , 5, 5)).to.equal("an equilateral");
     });
 });
