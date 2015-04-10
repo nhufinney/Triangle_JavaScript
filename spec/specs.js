5c090c1d31f64bcb74ba7d560c69ef4d7ef39f52
nhu finney <nhufinney@gmail.com>
2015-04-10 09:26:25 -0700

    passed the second test.

--- spec/specs.js
+++ spec/specs.js
@@ -3,5 +3,9 @@ describe('triangle', function(){
     it("it returns 3 lengthes do not form a triangle", function(){
         expect(triangle(1 , 8, 2)).to.equal("not triangle");
     });
+    
+    it("it returns 3 lengthes form a normal triangle: scalene", function(){
+        expect(triangle(5 , 8, 7)).to.equal("scalene");
+    });
 
 });
